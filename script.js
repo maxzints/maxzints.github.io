3bbf4fa6b3d66d0a3287c596565ac3bca81437bc
Zints <dzintsm@g.clemson.edu>
2025-11-09 13:49:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ function updateChart(rawData) {
     const simulation = d3.forceSimulation(filteredAndPivotedData)
         .force('x', d3.forceX(d => d.targetX).strength(0.15))
         .force('y', d3.forceY(d => d.targetY).strength(0.15))
-        .force('collide', d3.forceCollide(radius * 2 + 0.25)).on('tick', ticked); 
+        .force('collide', d3.forceCollide(radius * 2 + 0.25)); 
 
     simulation.stop();
     for (let i = 0; i < 150; ++i) { 
@@ -208,7 +208,7 @@ function updateChart(rawData) {
         .selectAll("text")
         .attr("x", -12) 
         .attr("y", -8)
-        .attr("transform", "rotate(-90)") 
+        .attr("transform", "rotate(-65)") 
         .style("text-anchor", "middle"); 
 
     svg.selectAll(".domain").attr("stroke", "none");
